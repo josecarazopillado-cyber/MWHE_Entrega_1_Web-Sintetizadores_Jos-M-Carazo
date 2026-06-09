daf49ab643801c6678570af1a1703713dbd45ec2
josecarazopillado-cyber <josecarazopillado@gmail.com>
2026-06-10 00:36:10 +0200

    soporte terminado

--- js/script.js
+++ js/script.js
@@ -15,4 +15,39 @@ $(document).ready(function() {
         $('#search-input').removeClass('open');
         }
     });
+
+    function filterProducts(filter) {
+        if (filter === 'todos') {
+            $('.product-item').fadeIn(300);
+        } else {
+            $('.product-item').each(function() {
+                if ($(this).data('category') === filter) {
+                    $(this).fadeIn(300);
+                } else {
+                    $(this).fadeOut(300);
+                }
+            });
+        }
+    }
+
+    // DROPDOWN FILTER
+    $('.filter-item').on('click', function() {
+        const filter = $(this).data('filter');
+        const label = $(this).text();
+
+        $('.filter-dd').text(label);
+
+        $('.filter-item').removeClass('active');
+        $(this).addClass('active');
+
+        filterProducts(filter);
+    });
+
+    // DESKTOP FILTER
+    $('.filter-tab').on('click', function() {
+        $('.filter-tab').removeClass('active');
+        $(this).addClass('active');
+        filterProducts($(this).data('filter'));
+    });
 });
+
